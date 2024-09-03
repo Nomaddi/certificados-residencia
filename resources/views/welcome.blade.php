d87e5b7032c65a85ddb1dac2aa6a885b4c59497a
anfecaba2 <andres@nomaddi.com>
2024-09-02 21:47:19 -0500

    prueba2.0

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -131,5 +131,6 @@
         </div>
         <h1>holaaaa mundo</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim non laboriosam quaerat corrupti dolorum nemo consequatur aliquam eligendi! Non, amet consequuntur odio blanditiis dolor reprehenderit optio animi porro harum quibusdam!</p>
+        <p>nada</p>
     </body>
 </html>
