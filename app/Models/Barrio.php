435a766a23c2ff856e26c0ed8a9b3adcb4a95c9a
Jonathan-C117 <jonathan@nomaddi.com>
2024-09-12 10:41:45 -0500

    creacion del M,C de Barrios

---
+++ app/Models/Barrio.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Barrio extends Model
+{
+    use HasFactory;
+
+    protected $fillable =[
+        "nombreBarrios",
+        "upz",
+        "tipo",
+        "sector"
+        
+    ];    
+    protected $hidden=[
+        'id_barrios'
+    ];
+}
